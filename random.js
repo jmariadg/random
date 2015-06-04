4dfe727066ae26f28db81618d96ab631c0039480
Jose Maria Dominguez <jmariadg@hotmail.com>
2015-06-04 11:22:50 +0200

    Random versión 2

--- random.js
+++ random.js
@@ -1,1 +1,3 @@
 /* Ejemplo aplicación random.js para trabajar con Git */
+/* Añado un segundo comentario para que aparezca como nueva línea tras haber hecho mi primer commit y cerrar la versión 1 */
+/* Por ahora utilizo este fichero sólo para ver el contro de versiones con Git */
